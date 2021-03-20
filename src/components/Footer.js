6ed62a8b727e368d27d5c3bbe01034916e1dca66
gleitons <31367377+gleitons@users.noreply.github.com>
2021-03-19 21:03:18 -0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -62,12 +62,12 @@ const Footer = class extends React.Component {
                   <ul className="menu-list">
                     <li>
                       <Link className="navbar-item" to="/blog">
-                        Latest Stories
+                        Notícias
                       </Link>
                     </li>
                     <li>
                       <Link className="navbar-item" to="/contact">
-                        Contact
+                        Contato
                       </Link>
                     </li>
                   </ul>

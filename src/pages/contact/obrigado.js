960a3c332b0c8ba997e5666572507ac83670006a
gleitons <31367377+gleitons@users.noreply.github.com>
2021-03-19 21:15:02 -0300

    Update and rename thanks.js to obrigado.js

--- src/pages/contact/obrigado.js
+++ src/pages/contact/obrigado.js
@@ -6,8 +6,8 @@ export default () => (
     <section className="section">
       <div className="container">
         <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <h1>Obrigado!</h1>
+          <p>Em breve você receberá uma resposta em seu e-mail!</p>
         </div>
       </div>
     </section>

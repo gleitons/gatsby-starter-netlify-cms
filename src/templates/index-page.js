3f271a23c69c6165f30ceb3751cbdf534cb72871
gleitons <31367377+gleitons@users.noreply.github.com>
2021-04-11 07:14:02 -0300

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -89,7 +89,7 @@ export const IndexPageTemplate = ({
                 <Features gridItems={intro.blurbs} />
                 <div className="columns">
                   <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
+                    <Link className="btn" to="/dicas">
                       Veja nossas Dicas
                     </Link>
                   </div>
@@ -100,7 +100,7 @@ export const IndexPageTemplate = ({
                   </h3>
                   <BlogRoll />
                   <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
+                    <Link className="btn" to="/resultados">
                       Veja mais
                     </Link>
                   </div>
